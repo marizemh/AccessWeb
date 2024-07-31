f0b6616cec5314191d8c0624692bb53ea7ad7101
mdmijares <marize.mijares@skydropx.com>
2024-07-31 10:16:19 -0400

    add files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RequestFormComponent } from './modules/access-request/request-form/request-form.component';
 import { ApprovalFormComponent } from './modules/approval/approval-form/approval-form.component';
+// Importa otros componentes según sea necesario
 
 const routes: Routes = [
   { path: 'access-request', component: RequestFormComponent },
   { path: 'approve-request', component: ApprovalFormComponent },
-  // otras rutas
+  // Otras rutas que puedas tener
+  { path: '', redirectTo: '/access-request', pathMatch: 'full' }, // Redirige a la ruta de solicitud de acceso por defecto
+  { path: '**', redirectTo: '/access-request' } // Maneja rutas no encontradas
 ];
 
 @NgModule({
