999ca1cec07e06d112188b4352055d96abc068b1
mdmijares <marize.mijares@skydropx.com>
2024-07-31 00:04:09 -0400

    add files

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json()); // Para poder parsear JSON en las solicitudes
 app.use('/api', accessRequestRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
